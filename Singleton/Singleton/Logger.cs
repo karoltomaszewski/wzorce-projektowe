018b7987dbbb1b2274b248a0638537a2e8d1dd22
karoltomaszewski <karoltomaszewskiit@gmail.com>
2024-06-02 20:18:42 +0200

    Singleton (Logger)

---
+++ Singleton/Singleton/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Logger
+    {
+        private static Logger instance = null;
+        private List<string> messages;
+
+        private Logger()
+        {
+            messages = new List<string>();
+        }
+
+        public static Logger getInstance()
+        {
+            if (instance == null)
+            {
+                createInstance();
+            }
+
+            return instance;
+        }
+
+        public void log(string message)
+        {
+            messages.Add(message);
+            // tutaj powinna znaleźć się logiką związana z logowaniem komunikatu
+        }
+
+        private static void createInstance()
+        {
+           instance = new Logger();
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = Logger.getInstance();
+
+            logger.log("test");
+        }
+    }
+}
